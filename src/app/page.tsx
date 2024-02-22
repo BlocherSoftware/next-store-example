329a0aa6ff28731889ef1f2f0b6b3097d82aeba6
BlocherSoftware <130656195+BlocherSoftware@users.noreply.github.com>
2024-02-22 13:34:32 +0100

    Task 3 (#10)
    * Create product list
    
    * change link
    
    * Add category
    
    * task-2
    
    * Change nav link name and add aria-label to pagination element
    
    * fix navigation
    
    * fix pagination + navigation
    
    * fix header
    
    * add category, collection page + integrate with graphql
    
    * test fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-export default function Home() {
+import { getProductList } from "@/api/products";
+import { ProductList } from "@/ui/organisms/ProductList";
+import { PRODUCT_PER_PAGE } from "@/utils/constants";
+
+export default async function Home() {
+	const products = await getProductList(PRODUCT_PER_PAGE, 0);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-5">
 			<div className="container mx-auto">
 				<h1 className="mb-5 text-4xl font-bold">Homepage</h1>
+				<ProductList products={products.data} />
 			</div>
 		</main>
 	);
