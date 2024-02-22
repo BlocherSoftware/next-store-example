94fa7d8f64b9c3f4ad21443fabd5d65c26e8c43b
BlocherSoftware <130656195+BlocherSoftware@users.noreply.github.com>
2024-02-22 19:13:09 +0100

    Task 3 (#21)
    * Create product list
    
    * change link
    
    * Add category
    
    * task-2
    
    * Change nav link name and add aria-label to pagination element
    
    * fix navigation
    
    * fix pagination + navigation
    
    * fix header
    
    * add category, collection page + integrate with graphql
    
    * test fix
    
    * test fix
    
    * test fix
    
    * pagination fix
    
    * hotfix
    
    * hotfix
    
    * test fix
    
    * add metadata
    
    * test fix
    
    * test fix
    
    * test fix

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -19,7 +19,9 @@ export const ProductListItemDescription = ({
 	return (
 		<div className="flex flex-col bg-slate-50 p-4">
 			<Link href={`/product/${slug}`}>
-				<h3 className="mb-1 text-lg font-semibold">{name}</h3>
+				<h3 className="mb-1 text-lg font-semibold" role="heading">
+					{name}
+				</h3>
 			</Link>
 
 			<p className="mb-2 text-sm text-slate-400">Category: {category[0].name}</p>
