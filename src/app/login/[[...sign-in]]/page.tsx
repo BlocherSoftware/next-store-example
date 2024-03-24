9e6a3d211d32322989898569eb55c68b725c8b0e
Dawid Blocher <dawid.blocher@gmail.com>
2024-03-24 13:01:46 +0100

    test

--- src/app/login/[[...sign-in]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function LoginPage() {
-    return <div className="flex justify-center">
-        <SignIn />
-    </div>
-}
--- src/app/register/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function RegisterPage() {
-    return <div className="flex justify-center">
-        <SignUp />
-    </div>
-}
