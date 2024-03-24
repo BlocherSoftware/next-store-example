0955224c6e19a0993c51aacd3e1cfcf256949f58
Dawid Blocher <dawid.blocher@gmail.com>
2024-03-24 13:03:17 +0100

    test

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Suspense } from "react";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { HeaderNavigation } from "@/ui/molecules/HeaderNavigation";
 import { Serachbar } from "@/ui/atoms/Searchbar";
 import { Cart } from "@/ui/atoms/Cart";
@@ -21,14 +20,7 @@ export const Header = () => {
 						<Serachbar />
 					</Suspense>
 					<Cart />
-					<div>
-						<SignedIn>
-							<UserButton userProfileMode="navigation" />
-						</SignedIn>
-						<SignedOut>
-							<SignInButton />
-						</SignedOut>
-					</div>
+
 				</div>
 
 			</div>
